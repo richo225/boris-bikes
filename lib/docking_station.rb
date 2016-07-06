9202fa72ae1cbfcaf54ba37bb38ce55ba054fee0
Richard Bates <richo225@gmail.com>
2016-07-06 17:50:52 +0100

    Add unit tests for an instance of a working bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+require_relative "bike.rb"
+
 class DockingStation
   def release_bike
+    Bike.new
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,15 +1,24 @@
 require "docking_station"
+require "bike"
 
-describe DockingStation do
-  it {respond_to(:release_bike)}
-end
-
-=begin
+#describe DockingStation do
+#  it {respond_to(:release_bike)}
+#end
 
 describe DockingStation do
   it "responds to release_bike" do
     expect(subject).to respond_to(:release_bike)
   end
+
+  it "releases a bike when release_bike called" do
+    expect(subject.release_bike).to be_a Bike
+  end
+  #expect an instance of dockingstation to return an instance of a bike when release bike called
+
+  it "releases a working bike" do
+    expect((subject.release_bike).working?).to eq true
+  end
+  #expect an instance of dockingstation to return a working bike when release bike called
 end
 
-=end
+#Add a test to your DockingStation specification that a) gets a bike, and then b) expects the bike to be working
