9484fab026b7421e4b6a45f57923d765f442debd
Richard Bates <richo225@gmail.com>
2016-07-06 15:51:05 +0100

    Add DockingStation unit test and release_bike method

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,15 @@
 require "docking_station"
 
 describe DockingStation do
-
+  it {respond_to(:release_bike)}
 end
+
+=begin
+
+describe DockingStation do
+  it "responds to release_bike" do
+    expect(subject).to respond_to(:release_bike)
+  end
+end
+
+=end
